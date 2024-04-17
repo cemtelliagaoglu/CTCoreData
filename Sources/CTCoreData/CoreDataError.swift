016842fbf72f704a54b1c5e5b4f165c1d49d5fea
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2024-04-17 21:40:29 +0200

    Create CoreDataError

---
+++ Sources/CTCoreData/CoreDataError.swift
@@ -0,0 +1,32 @@
+//
+//  CoreDataError.swift
+//
+//
+//  Created by Cem Telliagaoglu on 17.04.2024.
+//
+
+import Foundation
+
+public enum CoreDataError: Error {
+    
+    case create, fetch, read, update, delete, save, unknown
+
+    var customMessage: String {
+        switch self {
+        case .create:
+            return "Failed to create entity"
+        case .fetch:
+            return "Failed to fetch entity"
+        case .read:
+            return "Couldn't read entity"
+        case .update:
+            return "Failed to update context"
+        case .delete:
+            return "Failed to delete entity"
+        case .save:
+            return "An error occured while saving context"
+        default:
+            return "Unknown core data error"
+        }
+    }
+}
